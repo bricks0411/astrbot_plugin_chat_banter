ede704bbd8919dbdb5bfb9d30b5aa45373c53886
Bricks0411 <1072391442@qq.com>
2025-12-21 00:59:50 +0800

    fixed

--- main.py
+++ main.py
@@ -17,14 +17,24 @@ class RussianRoulette(Star):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
+    @filter.command("晚安", alias = {'安', 'goodnight', 'Goodnight', '晚安咯'})
     async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        """这是一个 晚安 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
         user_name = event.get_sender_name()
         message_str = event.message_str # 用户发的纯文本消息字符串
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        result = (
+            f"晚，晚安啦，{user_name}！\n"
+            "别误会，我可不是担心你，只是……今天看你还算努力。\n"
+            "早点睡，明天要是状态不好，可是会拖后腿的，知道吗？\n"
+            "……还有，别熬夜想些乱七八糟的事。\n"
+            "休息好，才、才不准做噩梦呢……\n"
+
+            "\n（小声）\n"
+            "……晚安。要是做梦的话，也给我做个像样点的。"
+        )
+        yield event.plain_result([result]) # 发送一条纯文本消息
 
     @filter.command("add")
     async def GetSum(self, event: AstrMessageEvent, a: int, b: int):
